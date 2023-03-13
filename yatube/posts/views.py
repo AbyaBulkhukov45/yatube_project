6b5a7596e34c436a1418850ff62f9a95ab48dc9d
PavelMinsky45 <Minsk5545@gmail.com>
2023-03-13 17:33:40 +0300

    начало финального проекта спринт 3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from .models import Post, Group
 
 
 # Create your views here.
 def index(request):
     template = 'posts/index.html'
-    return render(request, template, {})
+    title = 'YA Главная страница'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+        'title': title,
+        'text': 'Это главная страница Yatube',
+    }
+    return render(request, template, context, {})
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request,template,{'slug':slug})
+    title = ' YA Посты'
+    group = get_object_or_404(Group, slug=slug )
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group':group,
+        'posts':posts,
+        'title': title,
+        'text': 'Здесь будет информация о группах проекта Yatube'
+    }
+    return render(request,template,context,)
